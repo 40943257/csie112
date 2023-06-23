190f1f635bbce3cfabdaa21555b4c6bd4d5a75f9
40943257 <s43798591@gmail.com>
2023-06-23 21:45:50 +0800

    縣市陣列放在一個layout

---
+++ team10/view/layout/gov.php
@@ -0,0 +1,9 @@
+<?php
+$gov = [
+    '臺北市', 'TaipeiCity', '新北市', 'NewTaipeiCity', '基隆市', 'KeelungCity', '宜蘭縣', 'YilanCounty',
+    '桃園市', 'TaoyuanCity', '新竹縣', 'HsinchuCounty', '新竹市', 'HsinchuCity', '苗栗縣', 'MiaoliCounty',
+    '臺中市', 'TaichungCity', '彰化縣', 'ChanghuaCounty', '南投縣', 'NantouCounty', '雲林縣', 'YunlinCounty',
+    '嘉義縣', 'ChiayiCounty', '嘉義市', 'ChiayiCity', '台南市', 'TainanCity', '高雄市', 'KaohsiungCity',
+    '屏東縣', 'PingtungCounty', '花蓮縣', 'HualienCounty', '台東縣', 'TaitungCounty', '澎湖縣', 'PenghuCounty',
+    '金門縣', 'KinmenCounty', '連江縣', 'LienchiangCounty'
+];
