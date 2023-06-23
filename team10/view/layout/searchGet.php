074d1281ce726cc130d5904029ee3f69d1902d13
40943257 <s43798591@gmail.com>
2023-06-23 21:11:31 +0800

    關鍵字搜尋(機構名稱、地址、詳細資訊)裡有相關的字

--- team10/view/layout/searchGet.php
+++ team10/view/layout/searchGet.php
@@ -1,2 +1,6 @@
 <?php
-    $term = "";
+$term = "";
+$keywords = "";
+if (isset($_GET["keywords"]))
+    $keywords = $_GET["keywords"];
+$term .= "(name LIKE '%$keywords%' OR address LIKE '%$keywords%' OR detailed LIKE '%$keywords%')";
