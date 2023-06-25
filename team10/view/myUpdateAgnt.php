d86c4dfcd11e22288a2d682b781747fe94ed81cf
BCWizard <puhj97009@gmail.com>
2023-06-25 23:15:45 +0800

    實作更新商家資訊

--- team10/view/myUpdateAgnt.php
+++ team10/view/myUpdateAgnt.php
@@ -3,6 +3,7 @@ session_start();
 $conn=require_once "./layout/config.php";
 
 if ($_SERVER["REQUEST_METHOD"] == "POST"){
+    $id = $_POST["id"];
     $account = $_SESSION["account"];
 
     $name=$_POST["name"];                                       //機構名稱
@@ -24,6 +25,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST"){
         $govInput = $_POST['govInput'];
     $detailed=$_POST["detailed"];                               //詳細描述
 
+    echo "$id<br>";
     echo "機構名稱: $name <br>";
     echo "聯絡電話: $phone <br>";
     echo "機構地址: $address <br>";
@@ -39,7 +41,9 @@ if ($_SERVER["REQUEST_METHOD"] == "POST"){
 
     if(isset($admission_typeInput)){
         echo "長照對象:<br>";
+        $store_admission_typeInput = array();
         foreach($admission_typeInput as $admission_typeSelect){
+            $store_admission_typeInput[] = $admission_typeSelect;
             echo "&emsp;{$admission_typeSelect}<br>";
         }    
     }else{
@@ -74,21 +78,25 @@ if ($_SERVER["REQUEST_METHOD"] == "POST"){
 
     echo "詳細描述: $detailed <br>";
                                                                     //T10_agency_info
-    $sql_T10_agency_info = "INSERT INTO T10_agency_info (account, name, address, phone, start, end, people, detailed, review)
-                                VALUES ('{$account}', '{$name}', '{$address}', '{$phone}', '{$start}', '{$end}', '{$people}', '{$detailed}', false)";
+    $sql_T10_agency_info = "UPDATE T10_agency_info SET account = '{$account}', name = '{$name}', address = '{$address}', phone = '{$phone}', 
+                                                        start = '{$start}', end = '{$end}', people = '{$people}', detailed = '{$detailed}'
+                                                    WHERE id = '{$id}'";
     if(mysqli_query($conn,$sql_T10_agency_info)){   //檢查錯誤
-        echo "成功INSERT INTO T10_agency_info!<br>";
+        echo "成功UPDATE T10_agency_info!<br>";
     }
     else{                                           //有誤
         //echo "無法INSERT INTO T10_agency_info!<br>"; 
         echo mysqli_error($conn);
-        function_alert('無法INSERT INTO T10_agency_info!<br>');
+        function_alert('無法UPDATE T10_agency_info!<br>');
     }
 
-    $id = mysqli_insert_id($conn);                                  //取得剛剛insert into 的 id
-    //echo $id.'<br>';
                                                                     //T10_agency_care_type
     if(isset($care_typeInput)){
+        $sql_delete_care_type = "DELETE FROM T10_agency_care_type WHERE id = '{$id}'";
+        if(!mysqli_query($conn,$sql_delete_care_type)){   //檢查錯誤
+            echo mysqli_error($conn);
+            function_alert('無法DELETE T10_agency_care_type!<br>');
+        }
         foreach($care_typeInput as $care_typeSelect){
             $sql_T10_agency_care_type = "INSERT INTO T10_agency_care_type (id, care_type)
                                             VALUES ('{$id}', '{$care_typeSelect}')";
@@ -99,18 +107,24 @@ if ($_SERVER["REQUEST_METHOD"] == "POST"){
                 echo mysqli_error($conn);
                 function_alert('無法INSERT INTO T10_agency_care_type!<br>');
             }
-        }
-            
+        }       
     }
+
                                                                     //T10_agency_collect
-    for($i=1;$i<count($admission_type);$i+=2){
-        $admission_typeH = "h_{$admission_type["$i"]}";             //h_$English
-        $admission_typeM = "m_{$admission_type["$i"]}";             //m_$English
+    $sql_delete_agency_collect = "DELETE FROM T10_agency_collect WHERE id = '{$id}'";
+    if(!mysqli_query($conn,$sql_delete_agency_collect)){   //檢查錯誤
+        echo mysqli_error($conn);
+        function_alert('無法DELETE T10_agency_collect!<br>');
+    }
+
+    for($i=0;$i<count($store_admission_typeInput);$i+=1){
+        $admission_typeH = "h_{$store_admission_typeInput["$i"]}";             //h_$English
+        $admission_typeM = "m_{$store_admission_typeInput["$i"]}";             //m_$English
 
         if(!empty($_POST["$admission_typeH"])){
             $$admission_typeH = $_POST["$admission_typeH"];
             $sql_T10_agency_collect_h = "INSERT INTO T10_agency_collect (id, admission_type, money_flag, money)
-                                                                VALUES ('{$id}', '{$admission_type["$i"]}', 0, '{$$admission_typeH}')";
+                                                                VALUES ('{$id}', '{$store_admission_typeInput["$i"]}', 0, '{$$admission_typeH}')";
             if(mysqli_query($conn,$sql_T10_agency_collect_h)){   //檢查錯誤
                 echo "成功INSERT INTO T10_agency_collect_h!<br>";
             }
@@ -122,7 +136,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST"){
         if(!empty($_POST["$admission_typeM"])){
             $$admission_typeM = $_POST["$admission_typeM"];
             $sql_T10_agency_collect_m = "INSERT INTO T10_agency_collect (id, admission_type, money_flag, money)
-                                                                VALUES ('{$id}', '{$admission_type["$i"]}', 1, '{$$admission_typeM}')";
+                                                                VALUES ('{$id}', '{$store_admission_typeInput["$i"]}', 1, '{$$admission_typeM}')";
             if(mysqli_query($conn,$sql_T10_agency_collect_m)){   //檢查錯誤
                 echo "成功INSERT INTO T10_agency_collect_m!<br>";
             }
@@ -132,19 +146,36 @@ if ($_SERVER["REQUEST_METHOD"] == "POST"){
             }
         }
     }
+
                                                                     //T10_cooperative
-    foreach($govInput as $govInputSelect){
-        $sql_T10_cooperative = "INSERT INTO T10_cooperative (id, government)
-                                                    VALUES ('{$id}', '{$govInputSelect}')";
-        if(mysqli_query($conn,$sql_T10_cooperative)){   //檢查錯誤
-            echo "成功INSERT INTO T10_cooperative!<br>";
-        }
-        else{                                               //有誤
-            echo mysqli_error($conn);
-            function_alert('無法INSERT INTO T10_cooperative!<br>');
+    $sql_delete_T10_cooperative = "DELETE FROM T10_cooperative WHERE id = '{$id}'";
+    if(!mysqli_query($conn,$sql_delete_T10_cooperative)){   //檢查錯誤
+        echo mysqli_error($conn);
+        function_alert('無法DELETE T10_cooperative!<br>');
+    }
+    if(!empty($_POST['govInput'])){
+        foreach($govInput as $govInputSelect){
+            $sql_T10_cooperative = "INSERT INTO T10_cooperative (id, government)
+                                                        VALUES ('{$id}', '{$govInputSelect}')";
+            if(mysqli_query($conn,$sql_T10_cooperative)){   //檢查錯誤
+                echo "成功INSERT INTO T10_cooperative!<br>";
+            }
+            else{                                               //有誤
+                echo mysqli_error($conn);
+                function_alert('無法INSERT INTO T10_cooperative!<br>');
+            }
         }
     }
-
+    
+    $directory = "../image/agency/$id";
+    if (is_dir($directory)) {
+        $files = glob($directory . '/*'); // 取得資料夾中的所有檔案
+        foreach ($files as $file) {
+            if (is_file($file)) {
+                unlink($file); // 刪除檔案
+            }
+        }
+    }
     $targetDir = "../image/agency/".$id."/";                     // 指定的目標資料夾路徑
     if (!is_dir($targetDir)) {
         mkdir($targetDir, 0777, true);                          // 如果目標資料夾不存在，則建立資料夾
